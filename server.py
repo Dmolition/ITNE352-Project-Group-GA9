b512987136d2063db6f8d20d9f125ba06d3bfb92
Mariamtaleb <mariamtaleb121212@gmail.com>
2023-12-27 22:06:01 +0300

    code

--- server.py
+++ server.py
@@ -7,41 +7,54 @@ import threading
 # Function to handle client connection    part 4 threading function
 def handle_client(client_socket, client_name):
     print("Accepted connection from", client_name)
-
-    # Receive and process client requests
-    while True:
-        request = client_socket.recv(1024).decode('ascii')
-        if not request:
-            break
-        # Parse client request
-        request_parts = request.split(':')
-        request_type = request_parts[0]
-        request_params = request_parts[1:]
-
+    
+    try:
+        # Receive and process client requests
+        while True:
+            request = client_socket.recv(1024).decode('ascii')
+            if not request:
+                break
         # Perform the requested action and send response
-        if request_type == '1':
-            print(" Requester name: ",client_name ,"\n", "Type of request: All Arrived Flights","\n", "Request parameters: null")
-            response = get_all_arrived_flights()
-        elif request_type == '2':
-            print(" Requester name: ",client_name ,"\n", "Type of request: All Delayed Flights","\n", "Request parameters: null")
-            response = get_all_delayed_flights()
-        elif request_type == '3':
-            para = client_socket.recv(1024).decode('ascii')
-            print(" Requester name: ",client_name ,"\n", "Type of request: All Flights from Specific Airport","\n", "Request parameters:{}".format(para))
-            response = get_flights_from_airport(para)
-        elif request_type == '4':
-            para = client_socket.recv(1024).decode('ascii')
-            print(" Requester name: ",client_name ,"\n", "Type of request: Details of Specific Flight","\n", "Request parameters:{}".format(para))
-            response = get_flight_details(para)
-        else:
-            response = "Invalid request"
+            if request == '1':
+                print(" Requester name: ",client_name ,"\n", "Type of request: 1- All Arrived Flights","\n", "Request parameters: null")
+                response = get_all_arrived_flights()
+                client_socket.send(response.encode('ascii'))
+            elif request == '2':
+                print(" Requester name: ",client_name ,"\n", "Type of request: 2- All Delayed Flights","\n", "Request parameters: null")
+                response = get_all_delayed_flights()
+                client_socket.send(response.encode('ascii'))
+            elif request == '3':
+                sAirportpara = client_socket.recv(1024).decode('ascii')
+                print(" Requester name: ",client_name ,"\n", "Type of request: 3- All Flights from Specific Airport","\n", "Request parameters:{}".format(sAirportpara))
+                response = get_flights_from_airport(sAirportpara)
+                client_socket.send(response.encode('ascii'))
+            elif request == '4':
+                sFlightpara = client_socket.recv(1024).decode('ascii')
+                print(" Requester name: ",client_name ,"\n", "Type of request: 4- Details of Specific Flight","\n", "Request parameters:{}".format(sFlightpara))
+                response = get_flight_details(sFlightpara)
+                client_socket.send(response.encode('ascii'))
+            elif request == '5':
+            # Send a message to the client
+                response='Connection ended. Goodbye!'.encode('ascii')
+                client_socket.send(response)
+            # Close the client's connection
+                client_socket.close()
+                print(f"Connection closed with client: {client_name}")
+                return
+            else:
+                response = "Invalid request"
 
         # Send response to the client
         client_socket.send(response.encode('ascii'))
+    
+    except Exception as e:
+        print("An error occurred:", str(e))
+
 
     # Close the connection
-    print("Client", client_name, "disconnected")
-    client_socket.close()
+        print("Client", client_name, "disconnected")
+        print("\n"," ✈️ "*15,"\n")
+        client_socket.close()
 
 # Function to get all arrived flights
 def get_all_arrived_flights():
@@ -53,7 +66,7 @@ def get_all_arrived_flights():
     flights = data['data']
     arrived_flights = []
     for flight in flights:
-        if flight['arrival']['estimated'] is not None:
+        if flight['flight_status']== 'landed':
             arrived_flight = {
                 'Flight IATA Code': flight['flight']['iata'],
                 'Departure Airport': flight['departure']['airport'],
@@ -75,14 +88,14 @@ def get_all_delayed_flights():
     flights = data['data']
     delayed_flights = []
     for flight in flights:
-        if flight['departure']['estimated'] is not None and flight['departure']['estimated'] > flight['departure']['scheduled']:
+        if flight['arrival']['delay']!=None:
             delayed_flight = {
                 'Flight IATA Code': flight['flight']['iata'],
                 'Departure Airport': flight['departure']['airport'],
-                'Original Departure Time': flight['departure']['scheduled'],
+                'Original Departure Time': flight['arrival']['scheduled'],
                 'Estimated Arrival Time': flight['arrival']['estimated'],
                 'Arrival Terminal Number': flight['arrival']['terminal'],
-                'Delay': flight['departure']['estimated'] - flight['departure']['scheduled'],
+                'Delay': flight['arrival']['delay'],
                 'Arrival Gate': flight['arrival']['gate']
             }
             delayed_flights.append(delayed_flight)
@@ -99,11 +112,11 @@ def get_flights_from_airport(airport_icao):
     flights = data['data']
     airport_flights = []
     for flight in flights:
-        if flight['departure']['airport'] == airport_icao:
+        if flight['departure']['iata'] == airport_icao:
             airport_flight = {
                 'Flight IATA Code': flight['flight']['iata'],
                 'Departure Airport': flight['departure']['airport'],
-                'Original Departure Time': flight['departure']['estimated'],
+                'Original Departure Time': flight['departure']['actual'],
                 'Estimated Arrival Time': flight['arrival']['estimated'],
                 'Departure Gate': flight['departure']['gate'],
                 'Arrival Gate': flight['arrival']['gate'],
@@ -152,40 +165,46 @@ def server():
         'limit': 100
     }
 
+    try:
     # Send GET request to AviationStack API
-    api_url = 'http://api.aviationstack.com/v1/flights'
-    apiResponse = requests.get(api_url, params)
+        api_url = 'http://api.aviationstack.com/v1/flights'
+        apiResponse = requests.get(api_url, params)
 
-    # Process the response
-    if apiResponse.status_code == 200:
-        data = apiResponse.json()  # convert to json
+        # Process the response
+        if apiResponse.status_code == 200:
+            data = apiResponse.json()  # convert to json
 
-        # Store the data in a JSON file
-        file_path = 'group_GA9.json'
-        with open(file_path, 'w') as file:
-            json.dump(data, file, indent=3)
+            # Store the data in a JSON file
+            file_path = 'group_GA9.json'
+            with open(file_path, 'w') as file:
+                json.dump(data, file, indent=3)
 
-        print("Flight records saved to", file_path)
-    else:
-        print("Error occurred:", apiResponse.status_code)
+            print("Flight records saved to", file_path)
+        else:
+            print("Error occurred:", apiResponse.status_code)
 
-    # Set up server socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('127.0.0.1', 4444)
-    server_socket.bind(server_address)
-    server_socket.listen(3)
-    print("Server started. Listening for connections...")
+        # Set up server socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('127.0.0.1', 4444)
+        server_socket.bind(server_address)
+        server_socket.listen(3)
+        print("Server started. Listening for connections on {}".format(server_address))
 
-    # Handle client connections
-    while True:
-        client_socket, client_address = server_socket.accept()
+        # Handle client connections
+        while True:
+            client_socket, client_address = server_socket.accept()
 
-        # Receive client name
-        client_name = client_socket.recv(1024).decode('utf-8')
+            # Receive client name
+            client_name = client_socket.recv(1024).decode('ascii')
 
-        # Start a new thread to handle the client
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_name))
-        client_thread.start()
+            # Start a new thread to handle the client
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_name))
+            client_thread.start()
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+    server_socket.close()
 
 # Start the server
 server()
